715be1f694e027da2a23d61b8692c9b1f4fe34cd
manalyadav <manalyadav.0001@gmail.com>
2026-03-17 02:12:55 +0530

    [manalyadav]Package added

---
+++ Package/src/PackageAssignment/college/student/Students.java
@@ -0,0 +1,7 @@
+package PackageAssignment.college.student;
+public class Students{
+    public void displayStudent() {
+        System.out.println("Student Name: Manal Yadav");
+        System.out.println("Roll Number: 42");
+    }
+}
---
+++ Package/src/PackageAssignment/com/university/department/cse/Course.java
@@ -0,0 +1,4 @@
+package PackageAssignment.com.university.department.cse;
+
+public class Course {
+}
